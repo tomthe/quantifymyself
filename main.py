cb1b2516df9e8baecbb69972385bb8f5da91fee7
Tom Theile <tomthe@users.noreply.github.com>
2014-08-13 18:20:29 +0200

    created the DateSlider.. needs implementation into entryView

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ListProperty, StringProperty, ObjectProperty, NumericProperty
 from kivy.app import App
 from json import loads
+from time import strftime
+from datetime import datetime
 
 kivy.require('1.0.7')
 
@@ -30,10 +32,55 @@ class CustomSlider(BoxLayout):
         self.max = value[1]
         print self.range, self.min, self.sl.range, self.sl.min
 
+class DateSlider(BoxLayout):
+    syear = ObjectProperty()
+    smonth = ObjectProperty()
+    sday = ObjectProperty()
+    shour = ObjectProperty()
+    sminute = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(DateSlider, self).__init__(**kwargs)
+        now = datetime.now()
+        print "build dateslider...now:", now
+        self.syear.step = 1
+        self.syear.value = now.year
+        self.syear.min = now.year-2
+        self.syear.max = now.year+2
+
+        self.smonth.step = 1
+        self.smonth.value = now.month
+        self.smonth.min = 1
+        self.smonth.max = 12
+
+        self.sday.step = 1
+        self.sday.value = now.day
+        self.sday.min = 1
+        self.sday.max = 31
+
+        self.shour.step = 1
+        self.shour.value = now.hour
+        self.shour.min = 1
+        self.shour.max = 24
+
+        self.sminute.step = 1
+        self.sminute.value = now.minute
+        self.sminute.min = 1
+        self.sminute.max = 59
+
+    def get_time_tuple(self):
+        return [int(self.syear.value), int(self.smonth.value), int(self.sday.value), int(self.shour.value), int(self.sminute.value)]
+
+    def get_datetime(self):
+        return datetime(int(self.syear.value), int(self.smonth.value), int(self.sday.value), int(self.shour.value), int(self.sminute.value))
+
+    def test(self):
+        print self.syear,self.syear.value
+        print self.get_datetime()
 
 class EnterView(BoxLayout):
     dict={}
-
+    parent_button_view =None
     val1 = NumericProperty()
     val1text = StringProperty("no String")
     val2 = NumericProperty()
@@ -44,10 +91,17 @@ class EnterView(BoxLayout):
         print "build EnterView --- dict:    ", self.dict
 
         self.clear_widgets()
+        #----------
+        #datetime...
+        dsstart = DateSlider()
+        self.add_widget(dsstart)
+        #now.
+
+#----------
+        label_text= Label()
         if 'text' in self.dict:
-            label_text= Label()
             label_text.text = self.dict['text']
-            self.add_widget(label_text)
+        self.add_widget(label_text)
         if 'val1range' in self.dict:
             cslider1 = CustomSlider()
             cslider1.range=self.dict['val1range']
@@ -70,8 +124,14 @@ class EnterView(BoxLayout):
 
     def cancelf(self,instance):
         print "cancel..."
+        self.parent_button_view.clear_widgets()
+        self.parent_button_view.show_first_level()
 
     def log_entry(self,instance):
+        new_log_entry = {}
+
+
+        self.parent_button_view.log.append(new_log_entry)
         print "log..."
 
 class NewEnterView(BoxLayout):
@@ -120,6 +180,7 @@ class NewEnterView(BoxLayout):
 
 class ButtonView(BoxLayout):
     button_dict =[]#[{'type':'submenu','text':'sleep','children':[{'type':'log','text':'sleep start'},{'type':'log','text':'sleep stop'}]} ]
+    app = None
     log=[]
 
     def build(self):
@@ -154,6 +215,7 @@ class ButtonView(BoxLayout):
     def add_add_button(self,dict):
         bt = Button(text="+")
         bt.dict = dict
+        bt.parent_button_view = self
         bt.bind(on_press=self.add_button)
         self.add_widget(bt)
 
@@ -176,6 +238,7 @@ class ButtonView(BoxLayout):
             self.clear_widgets()
             ev = EnterView()
             ev.dict=instance.dict
+            ev.parent_button_view=self
             ev.build()
             print "ev..............", ev
             self.add_widget(ev)
@@ -187,6 +250,8 @@ class QuantifyApp(App):
     def build(self):
         bv = ButtonView(button_dict=self.button_dict,log=self.log)
         bv.button_dict = self.button_dict
+        bv.log = self.log
+        bv.app=self
         print "build app...", self.button_dict
         bv.show_first_level()
         return bv
